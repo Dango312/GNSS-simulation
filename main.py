733e855ce739290e6b41e92946b687166913f588
Dango312 <mankevich.sergeyint@gmail.com>
2024-05-03 22:02:18 +0300

    Сhanging theme and new information in logs

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import csv
 
 
 class App(ttk.Window):
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         super().__init__(themename='darkly')
 
         self.columnconfigure(0, weight=1)
@@ -24,6 +24,7 @@ class App(ttk.Window):
     def draw_menu(self):
         """
         Draw top menu
+
         :return:
         """
         menu = ttk.Menu(self)
@@ -36,8 +37,7 @@ class App(ttk.Window):
         settings_menu = ttk.Menu(menu, tearoff=False)
         settings_menu.add_command(label='Dark theme', command=self.set_dark_theme)
         settings_menu.add_command(label='Light theme', command=self.set_light_theme)
-        settings_menu.add_command(label='About', command=self.open_about)
-        menu.add_cascade(label='Settings', menu=settings_menu)
+        menu.add_cascade(label='Change theme', menu=settings_menu)
 
         computation_menu = ttk.Menu(menu, tearoff=False)
         computation_menu.add_command(label='Compute', command=self.compute)
@@ -50,15 +50,10 @@ class App(ttk.Window):
     def set_light_theme(self):
         self.style.theme_use('cosmo')
 
-    def open_settings(self):
-        pass
-
-    def open_about(self):
-        print('about')
-
     def save_params(self):
         """
         Save parameters into .csv file
+
         :return:
         """
         filepath = tk.filedialog.asksaveasfilename(initialfile='parameters.csv',
@@ -74,11 +69,11 @@ class App(ttk.Window):
                 writer.writerow(data[1]) # receiver coordinates
                 writer.writerow([data[2]]) # tau
                 writer.writerow(data[3]) # ri
-        print('File saved')
 
     def open_params(self):
         """
         Open parameters from .csv file
+
         :return:
         """
         filepath = tk.filedialog.askopenfilename(defaultextension=".csv",
@@ -91,21 +86,19 @@ class App(ttk.Window):
             data.append((list_reader[2])) # receiver coordinates
             data.append(list_reader[3]) # tau
             data.append(list_reader[4]) # ri
-        print(data)
         try:
             self.beacon_frame.insert_data(data)
         except IndexError:
             self.beacon_frame.show_warnings('insufficient_entries')
-        print('File opened')
 
     def compute(self):
         """
-        Computate distances
+        Compute distances
+
         :return: None
         """
         try:
             beacon_coords, receiver_coords, tau, ri = self.beacon_frame.get_data()
-            print("Coordinate:", beacon_coords, receiver_coords, tau, ri)
             beacons = Beacons(coordinates=beacon_coords, receiver_x=receiver_coords[0], receiver_y=receiver_coords[1],
                               tau=tau, ri=ri)
         except ValueError:
@@ -121,6 +114,7 @@ class App(ttk.Window):
     def save_logs(self):
         """
         Save logs in .txt file
+
         :return:
         """
         filepath = tk.filedialog.asksaveasfilename(initialfile='logs.txt',
@@ -131,6 +125,12 @@ class App(ttk.Window):
             local_time = time.localtime(current_time)
             formatted_time = time.strftime("%Y-%m-%d %H:%M:%S\n", local_time)
             f.write(formatted_time)
+
+            data = self.beacon_frame.get_data()
+            for i in range(len(data[0])):
+                f.write(f"Beacon {i+1} coordinates: {data[0][i]}, ri={data[3][i]}\n")
+            f.write(f"Receiver coordinates: {data[1]}, tau={data[2]}\n")
+
             f.write(self.logs_frame.log_window['text'])
             f.close()
 
@@ -141,7 +141,7 @@ class LogFrame(ttk.Frame):
         self.grid(row=0, column=1, sticky='nsew')
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
-        self.log_window = ttk.Label(self, text='logs', justify=ttk.LEFT, relief=ttk.GROOVE,
+        self.log_window = ttk.Label(self, justify=ttk.LEFT, relief=ttk.GROOVE,
                                     font='Courier 10', name='logs')
         self.log_window.grid(row=0, column=0, sticky='nsew')
 
@@ -156,6 +156,7 @@ class LogFrame(ttk.Frame):
         self.log_window.config(text=logs)
 
 
+# Frame with simulation parameters
 class ParametersFrame(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
@@ -164,7 +165,7 @@ class ParametersFrame(ttk.Frame):
         self.columnconfigure(1, weight=1)
         self.rowconfigure([i for i in range(3)], weight=1)
         for i in range(1, 4):
-            self.create_beacon_entry(i, f'r{i}').grid(row=i-1, column=0)
+            self.create_beacon_entry(i, i).grid(row=i-1, column=0)
 
         self.create_receiver_entry().grid(row=0, column=1)
 
@@ -174,24 +175,29 @@ class ParametersFrame(ttk.Frame):
                                      command=self.add_beacon, bootstyle=ttk.PRIMARY).grid(row=2, column=1)
 
     def add_beacon(self):
+        """
+        Create entries for new beacon
+
+        :return:
+        """
         beacons = self.grid_size()[1]
         self.rowconfigure(beacons, weight=1)
-        self.create_beacon_entry(beacons+1, f'r{beacons}').grid(row=beacons, column=0)
+        self.create_beacon_entry(beacons+1, beacons+1).grid(row=beacons, column=0)
 
-    def create_beacon_entry(self, number: int, error_text: str) -> ttk.Frame:
+    def create_beacon_entry(self, beacon_i: int, r_i: str) -> ttk.Frame:
         """
         Create frame with entries for beacon
 
-        :param number:
-        :param error_text:
+        :param beacon_i:
+        :param r_i:
         :return: Frame with entries for beacon
         """
-        beacon_frame = ttk.Frame(self, name=f'beacon_frame{number}')
+        beacon_frame = ttk.Frame(self, name=f'beacon_frame{beacon_i}')
         self.columnconfigure((0, 1, 2), weight=1)
         self.rowconfigure((0, 1), weight=1)
 
-        beacon_label = ttk.Label(beacon_frame, text=f"beacon {number} coordinates")
-        ri_label = ttk.Label(beacon_frame, text=error_text)
+        beacon_label = ttk.Label(beacon_frame, text=f"beacon {beacon_i} coordinates")
+        ri_label = ttk.Label(beacon_frame, text=f'r{r_i}')
         x_entry = ttk.Entry(beacon_frame, width=10, name='x_entry')
         y_entry = ttk.Entry(beacon_frame, width=10, name='y_entry')
         ri_entry = ttk.Entry(beacon_frame, width=10, name='ri_entry')
@@ -206,6 +212,7 @@ class ParametersFrame(ttk.Frame):
     def create_receiver_entry(self) -> ttk.Frame:
         """
         Create frame with entries for receiver data
+
         :return:
         """
         receiver_frame = ttk.Frame(self, name='receiver_frame')
@@ -226,6 +233,11 @@ class ParametersFrame(ttk.Frame):
         return receiver_frame
 
     def create_epoch_entry(self):
+        """
+        Create frame with entries for epochs
+
+        :return:
+        """
         epoch_frame = ttk.Frame(self, name='epoch_frame')
         self.columnconfigure(0, weight=1)
         self.rowconfigure((0, 1), weight=1)
@@ -237,9 +249,10 @@ class ParametersFrame(ttk.Frame):
         self.epochs_entry.grid(row=1, column=0, sticky='nsew')
         return epoch_frame
 
-    def get_data(self):
+    def get_data(self) -> list():
         """
         Get data from entries
+
         :return: beacon_coordinates (list[tuple]), (receiver_x, receiver_y), tau (int), ri (list)
         """
         beacons_coordinates = []
@@ -258,14 +271,12 @@ class ParametersFrame(ttk.Frame):
                 tau = self.children[c].children['tau'].get()
         return beacons_coordinates, (float(receiver_x), float(receiver_y)), float(tau), ri_data
 
-    def insert_data(self, data):
+    def insert_data(self, data: list):
         """
         Insert data in entries
         :param data: data to insert
         :return:
         """
-        print(self.children.keys())
-        print(self.children['beacon_frame1'].children)
         beacon_frames = [key for key in self.children.keys() if key.startswith('beacon_frame')]
         # insert receiver data
         self.children['receiver_frame'].children['receiver_x'].delete(0, last=ttk.END)
@@ -287,11 +298,12 @@ class ParametersFrame(ttk.Frame):
     def get_epoch(self) -> int:
         """
         Return the entered number of epochs
+
         :return: int: epochs
         """
         return int(self.epochs_entry.get())
 
-    def show_warnings(self, warning_type):
+    def show_warnings(self, warning_type: str):
         if warning_type == 'beacon_input':
             tk.messagebox.showwarning(title="Warning",
                                       message="Invalid input format.\n"
